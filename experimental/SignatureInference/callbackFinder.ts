d408ef3b686ff8fb4f42a1c07bf26ad1ab452a92
robertDurst <me@robdurst.com>
2019-10-22 17:26:41 -0400

    re-org files in directory

--- experimental/SignatureInference/callbackFinder.ts
+++ experimental/SignatureInference/callbackFinder.ts
@@ -2,4 +2,4 @@ import {Source} from './utils';
 
 const rawSource = `import { map } from 'lodash'; map()`;
 const src = new Source(rawSource);
-console.log(src.getSignatureInfo());
+console.log(src.getSignatureInfo().items[0].parameters.forEach(x => console.log(x)));
